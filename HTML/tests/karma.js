a2b3f1970a732ec6805eb005f12991cc9b0435f4
Brett Cannon <brett@python.org>
2013-09-02 11:37:41 -0400

    Add a configuration file for Karma

---
+++ HTML/tests/karma.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Mon Sep 02 2013 11:03:12 GMT-0400 (EDT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '..',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      '*.js',
+      'assets/*.js',
+      'assets/oplop/*.js',
+      'assets/jquery.mobile/jquery-*.js',
+      'tests/lib/*.js',
+      'tests/specs/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+       'assets/startup.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+        // Only list multi-OS browsers; override as desired with the
+        // --browser command-line option to add OS-specific browsers.
+        'Chrome',
+        'Firefox',
+        'PhantomJS'
+    ],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
