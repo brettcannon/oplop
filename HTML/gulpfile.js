56038729b55768eea63edcc0829864e7b16f8a11
Brett Cannon <brett@python.org>
2015-06-01 10:01:35 -0400

    Introduce service workers
    Provides more robust offline support and allows for the “Add to
    Homescreen” banner under Chrome for Android (although the app cache
    solution for offline is kept for non-Blink browsers).
    
    Also led to switching from custom Python scripts for build maintenance
    to Gulp.

---
+++ HTML/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    fs = require('fs'),
+    manifest = require('gulp-manifest'),
+    swPrecache = require('sw-precache');
+
+gulp.task('default', ['generate-service-worker', 'manifest']);
+
+gulp.task('generate-service-worker', function(callback) {
+  swPrecache({
+    staticFileGlobs: ['*.html', 'assets/**/*',
+        'service-worker-registration.js', 'service-worker.js']
+  }, function(error, swFileContents) {
+    if (error) {
+      return callback(error);
+    }
+    fs.writeFile('service-worker.js', swFileContents, callback);
+  });
+});
+
+gulp.task('manifest', function() {
+  // gulp.src() strips the leading directory when, e.g. assets/**
+  // is used, so have to do more of a blacklist solution than a
+  // whitelist.
+  gulp.src([
+          '**/*.{css,gif,html,js,png,svg,woff}',
+          '!AppEngine/**', '!tests/**', '!node_modules/**'])
+      .pipe(manifest({
+          exclude: ['cache.manifest', 'gulpfile.js'],
+          filename: 'cache.manifest',
+          timestamp: false,
+          hash: true}))
+      .pipe(gulp.dest(''));
+});
